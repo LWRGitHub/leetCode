4f2c7d1094679ae0b40dbf0631da277a3d59aa18
LWRGitHub <LoganReynoldsLWR@gmail.com>
2023-02-25 21:25:00 -0800

    Improve trapping rain PY

--- python/trapping_rain_water.py
+++ python/trapping_rain_water.py
@@ -23,19 +23,19 @@ Constraints:
 LeetCode (2023) Trapping Rain Water; https://leetcode.com/problems/trapping-rain-water/
 """
 
-def trap(height):
+def trap(height, watter=0):
 
     n = len(height)
     left = [0] * n
+    left[0] = height[0]
     right = [0] * n
+    right[n-1] = height[n-1]
     
     for i in range(n):
         left[i] = max(left[i-1], height[i])
-    for i in range(n):
-        idx = n - i - 1
-        right[idx] = max(right[idx+1 if idx+1 != n else idx], height[idx])
+    for i in range(n-2, -1, -1):
+        right[i] = max(right[i+1], height[i])
         
-    watter = 0
     for i in range(n):
         watter += min(left[i], right[i]) - height[i]
     return watter
