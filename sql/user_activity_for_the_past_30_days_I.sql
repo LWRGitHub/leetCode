bd225c50ef2de29e45e3053b37ac03bb1c553af4
LWRGitHub <LoganReynoldsLWR@gmail.com>
2023-03-30 15:05:17 -0700

    user act 30 days table data SQL

--- sql/user_activity_for_the_past_30_days_I.sql
+++ sql/user_activity_for_the_past_30_days_I.sql
@@ -57,14 +57,39 @@
 */
 
 
+-- creating Activity
+CREATE TABLE Activity (
+    user_id INTEGER NOT NULL,
+    session_id INTEGER NOT NULL,
+    activity_date DATE NOT NULL,
+    activity_type varchar(12) not null
+);
+
+--  Activity added
+INSERT INTO Activity
+    (user_id, session_id, activity_date, activity_type)
+VALUES
+    (1, 1, '2019-07-20', 'open_session'),
+    (1, 1, '2019-07-20', 'scroll_down'),
+    (1, 1, '2019-07-20', 'end_session'),
+    (2, 4, '2019-07-20', 'open_session'),
+    (2, 4, '2019-07-21', 'send_message'),
+    (2, 4, '2019-07-21', 'end_session'),
+    (3, 2, '2019-07-21', 'open_session'),
+    (3, 2, '2019-07-21', 'send_message'),
+    (3, 2, '2019-07-21', 'end_session'),
+    (4, 3, '2019-06-25', 'open_session'),
+    (4, 3, '2019-06-25', 'end_session')
+;
+
 
 SELECT 
-  activity_date AS day, 
-  COUNT(DISTINCT user_id) AS active_users
+    activity_date AS day, 
+    COUNT(DISTINCT user_id) AS active_users
 FROM 
-  Activity
+    Activity
 WHERE (
-  activity_date > '2019-06-27' AND 
-  activity_date <= '2019-07-27'
+    activity_date > '2019-06-27' AND 
+    activity_date <= '2019-07-27'
 )
 GROUP BY activity_date;
