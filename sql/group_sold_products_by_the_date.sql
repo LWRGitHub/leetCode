491514b8930b40a1310c48572d968d2e17b9d677
LWRGitHub <LoganReynoldsLWR@gmail.com>
2023-04-05 20:10:57 -0700

    group products by date code  SQL

--- sql/group_sold_products_by_the_date.sql
+++ sql/group_sold_products_by_the_date.sql
@@ -55,3 +55,18 @@
 
     LeetCode (2023) Group Sold Products By The Date: https://leetcode.com/problems/group-sold-products-by-the-date/
 */
+
+select 
+    sell_date, 
+    count( 
+        DISTINCT product 
+    ) as num_sold ,
+    GROUP_CONCAT( 
+        DISTINCT product 
+        order by product 
+        ASC separator ',' 
+    ) as products
+FROM 
+    Activities 
+        GROUP BY sell_date 
+        order by sell_date ASC;
