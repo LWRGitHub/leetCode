76338abed85cab57035626a55478d0dac31a2468
LWRGitHub <LoganReynoldsLWR@gmail.com>
2024-10-23 12:27:52 -0700

    PY Same Tree

--- python/same_tree.py
+++ python/same_tree.py
@@ -24,6 +24,8 @@
         The number of nodes in both trees is in the range [0, 100].
         -10^4 <= Node.val <= 10^4
 
+    LeetCode (2024)  100. Same Tree: https://leetcode.com/problems/same-tree/
+
 """
 
 
